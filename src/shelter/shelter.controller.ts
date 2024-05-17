c01b704c41c71e5b15a3b97ab33599a35741577f
boginni <brunnomarqueszr1@gmail.com>
2024-05-16 22:09:29 -0300

    fix: importing from missing lib

--- src/shelter/shelter.controller.ts
+++ src/shelter/shelter.controller.ts
@@ -1,28 +1,30 @@
-import { LoginDto } from '@/auth/dtos/login.dto'
 import { Controller, Post } from '@nestjs/common'
-import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
-import { AddressDto } from "@/address/dtos/address.dto";
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
+import { AddressDto } from '@/address/dtos/address.dto'
+import { SignInDto } from '@/auth/dtos/sign-in.dto'
 
 @ApiTags('Shelter')
 @Controller('shelter')
 export class ShelterController {
   constructor() {}
+
   @ApiOkResponse({
     description: 'The user records',
     type: AddressDto,
     isArray: true,
   })
   @Post('')
-  async get(): Promise<LoginDto> {
+  async get(): Promise<SignInDto> {
     throw new Error('Not implemented')
   }
+
   @ApiOkResponse({
     description: 'The user records',
     type: AddressDto,
     isArray: true,
   })
   @Post(':id')
-  async getById(): Promise<LoginDto> {
+  async getById(): Promise<SignInDto> {
     throw new Error('Not implemented')
   }
 }
