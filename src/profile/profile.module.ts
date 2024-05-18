0aec4993d31bd3897827d2b23e6002fa3363a251
boginni <brunnomarqueszr1@gmail.com>
2024-05-18 15:44:29 -0300

    refactor: remove sub profile routes

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -6,17 +6,9 @@ import { AuthModule } from '@/auth/auth.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AddressEntity } from '@/core/entities/address.entity'
 import { UserEntity } from '@/core/entities/user.entity'
-import { ProfilePetsController } from '@/profile/controllers/profile-pets.controller'
-import { ProfileAddressesController } from '@/profile/controllers/profile-addresses.controller'
-import { ProfileSheltersController } from '@/profile/controllers/profile-shelters.controller'
 
 @Module({
-  controllers: [
-    ProfileController,
-    ProfilePetsController,
-    ProfileAddressesController,
-    ProfileSheltersController,
-  ],
+  controllers: [ProfileController],
   exports: [],
   imports: [
     CoreModule,
