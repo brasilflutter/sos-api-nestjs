b3ec4658da4559bb1a740f53e651909c62cf0be0
boginni <brunnomarqueszr1@gmail.com>
2024-05-18 13:09:15 -0300

    feat: implement methods for profile

---
+++ src/profile/controllers/profile-addresses.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Inject,
+  Put,
+  UseGuards,
+} from '@nestjs/common'
+import { ApiOkResponse, ApiSecurity, ApiTags } from '@nestjs/swagger'
+import { AddressDto } from '@/address/dtos/address.dto'
+import { ProfileService } from '@/profile/services/profile.service'
+import { AuthGuard } from '@/core/guards/auth.guard'
+import { REQUEST } from '@nestjs/core'
+import { UserEntity } from '@/core/entities/user.entity'
+import { Either } from '@/core/adapters/either'
+
+@ApiSecurity('bearer')
+@ApiTags('Profile')
+@UseGuards(AuthGuard)
+@Controller('profile')
+export class ProfileAddressesController {
+  constructor(
+    private readonly profileService: ProfileService,
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
+  @ApiOkResponse({
+    description: 'The user profile',
+    type: AddressDto,
+    isArray: true,
+  })
+  @Get('')
+  async get(): Promise<Either<HttpException, UserEntity>> {
+    return this.profileService.get()
+  }
+
+  @ApiOkResponse({
+    description: 'The user profile',
+    type: AddressDto,
+    isArray: true,
+  })
+  @Put('')
+  async put(
+    @Body() userEntity: Partial<UserEntity>,
+  ): Promise<Either<HttpException, UserEntity>> {
+    return this.profileService.update(userEntity)
+  }
+
+  @ApiOkResponse({
+    description: 'The user profile',
+    type: AddressDto,
+    isArray: true,
+  })
+  @Delete('')
+  async delete(): Promise<Either<HttpException, any>> {
+    return this.profileService.delete()
+  }
+}
--- src/profile/services/profile.service.ts
+++ src/profile/services/profile.service.ts
@@ -1,8 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { HttpException, Inject, Injectable } from '@nestjs/common'
 import { REQUEST } from '@nestjs/core'
 import { InjectRepository } from '@nestjs/typeorm'
 import { AddressEntity } from '@/core/entities/address.entity'
 import { Repository } from 'typeorm'
+import { UserEntity } from '@/core/entities/user.entity'
+import { Either, Left, Right } from '@/core/adapters/either'
+import { getAuthorization } from '@/core/adapters/getAuthorization'
 
 @Injectable()
 export class ProfileService {
@@ -11,9 +14,55 @@ export class ProfileService {
     private readonly request: Request,
     @InjectRepository(AddressEntity)
     private readonly addressRepository: Repository<AddressEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  async get(): Promise<any> {
-    return this.addressRepository.find()
+  async get(): Promise<Either<HttpException, UserEntity>> {
+    const authDto = getAuthorization(this.request)
+
+    const user = await this.userRepository.findOne({
+      where: { id: authDto.id },
+    })
+
+    if (!user) {
+      return new Left(new HttpException('User not found', 404))
+    }
+
+    return new Right(user)
+  }
+
+  async update(
+    data: Partial<UserEntity>,
+  ): Promise<Either<HttpException, UserEntity>> {
+    const authDto = getAuthorization(this.request)
+
+    const user = await this.userRepository.findOne({
+      where: { id: authDto.id },
+    })
+
+    if (!user) {
+      return new Left(new HttpException('User not found', 404))
+    }
+
+    await this.userRepository.update(user.id, data)
+
+    return new Right({ ...user, ...data })
+  }
+
+  async delete(): Promise<Either<HttpException, any>> {
+    const authDto = getAuthorization(this.request)
+
+    const user = await this.userRepository.findOne({
+      where: { id: authDto.id },
+    })
+
+    if (!user) {
+      return new Left(new HttpException('User not found', 404))
+    }
+
+    await this.userRepository.delete(user.id)
+
+    return new Right({})
   }
 }
